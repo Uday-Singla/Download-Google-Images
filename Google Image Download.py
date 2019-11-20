a0c65503da691f2663b1e3f767bd659e31e32a14
Uday-Singla <56470095+Uday-Singla@users.noreply.github.com>
2019-11-20 21:34:09 +0530

    specify keyword, number of photos

---
+++ Google Image Download.py
@@ -0,0 +1,51 @@
+# importing google_images_download module 
+from google_images_download import google_images_download
+
+# creating object 
+response = google_images_download.googleimagesdownload()
+
+search_queries = []
+
+search_queries.append(input("Enter keyword : "))
+num = int(input("Number of photos : "))
+
+def downloadimages(query):
+    # keywords is the search query 
+    # format is the image file format 
+    # limit is the number of images to be downloaded 
+    # print urs is to print the image file url 
+    # size is the image size which can 
+    # be specified manually ("large, medium, icon") 
+    # aspect ratio denotes the height width ratio 
+    # of images to download. ("tall, square, wide, panoramic") 
+    arguments = {"keywords": query,
+                 "format": "jpg",
+                 "limit": num,
+                 "print_urls": True,
+                 "size": "medium",
+                 "aspect_ratio": "panoramic",
+                 "output_directory": r"C:\Users\user\Desktop\Images_from_script"}
+    try:
+        response.download(arguments)
+
+        # Handling File NotFound Error
+    except FileNotFoundError:
+        arguments = {"keywords": query,
+                     "limit": num,
+                     "print_urls": True,
+                     "size": "medium",
+                     "output_directory": r"C:\Users\user\Desktop\Images_from_script"}
+
+        # Providing arguments for the searched query 
+        try:
+            # Downloading the photos based 
+            # on the given arguments 
+            response.download(arguments)
+        except:
+            pass
+
+
+# Driver Code
+for query in search_queries:
+    downloadimages(query)
+    print()  
